9bacaad931177aa5cac7b56e4866030c18837da4
Kiran Deepak Capoor <kiran.capoor94@gmail.com>
2019-11-22 21:56:19 +0530

    Added Navbar on side

--- Assets/js/buttons.js
+++ Assets/js/buttons.js
@@ -2,7 +2,7 @@ import {
     MDCRipple
 } from '@material/ripple/index.js';
 
-const selector = '.mdc-button, mdc-top-app-bar__action-item, .mdc-card__primary-action, #drawerList, mdc-icon-button, .mdc-radio, .mdc-list';
+const selector = '.mdc-button, mdc-top-app-bar__action-item, .mdc-card__primary-action, #drawerList, .mdc-radio, .mdc-list';
 const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
     return new MDCRipple(el);
 });
